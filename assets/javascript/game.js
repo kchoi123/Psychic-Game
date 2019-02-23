3fe214ca6a9674f98194780617d8c5f7189991a5
kchoi123 <k3vinch0i@gmail.com>
2019-02-22 18:56:49 -0800

    created a function to reset and cleaned up JS code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 // word choices
 var wordChoices = ["a", "b", "c", "d", 
     "e", "f", "g", "h", "i", "j", "k", 
@@ -7,7 +6,7 @@ var wordChoices = ["a", "b", "c", "d",
     "z"];
 var userChoice = [];
 
-// play results
+// first set of variables
 var wins = 0;
 var losses = 0;
 var guessLeftOver = 9;
@@ -18,10 +17,20 @@ var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessLeft = document.getElementById("guessLeft-text");
 var compChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-console.log(compChoice);
 
-//guess count
+// view text
+userGuessText.textContent = "Your guess so far: " + userChoice;
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+guessLeft.textContent = "Guesses Left: " + guessLeftOver;
 
+// reset function
+function resetGame() {
+    guessLeftOver = 9;
+    compChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+    userGuess = "";
+    userChoice = [];
+}
 
 // player input
 document.onkeyup = function(event) {
@@ -31,11 +40,7 @@ document.onkeyup = function(event) {
         compChoice.indexOf(userGuess) >= 0;
         alert("you win!");
         wins++;
-        guessLeftOver = 9;
-        compChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-        userGuess = "";
-        userChoice = [];
-        console.log(compChoice);
+        resetGame();
     } else if ((userGuess !== compChoice) && (guessLeftOver > 1)) {
         compChoice.indexOf(userGuess) >= 0;
         guessLeftOver--; 
@@ -43,11 +48,7 @@ document.onkeyup = function(event) {
     } else {
         alert("You Lose");
         losses++;
-        guessLeftOver = 9;
-        compChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-        userChoice = [];
-        userGuess = "";
-        console.log(compChoice);
+        resetGame();
     }
 
     // display
